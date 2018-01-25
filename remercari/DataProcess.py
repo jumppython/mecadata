da40478b530338ee148247545098919022d3b816
jumppython <jumppython@gmail.com>
2018-01-25 21:46:15 +0900

    DataFile update at 0124 and reset delay time of areainfo to 5.0

--- remercari/DataProcess.py
+++ remercari/DataProcess.py
@@ -4,20 +4,21 @@ import os
 from datetime import datetime
 
 class DataFile:
-	# Defind the data file to be processed by:
-	#   prefix: String type. If it is datetime string, the value of 'prefix_is_datetime_str' should be set to True.
-	#   suffix: String type. If it is datetime string, the value of 'suffix_is_datetime_str' should be set to True.
-	#   extension: String type. Designation the extension of file. Default value is '.csv' means CSV file.
-	#   By attributes dictionary, other file attributions, such as size, created time, etc, can be declarated for files filter.
+	"""
+	Defind the data file to be processed by:
+	   prefix: String type. If it is datetime string, the value of 'prefix_is_datetime_str' should be set to True.
+	   suffix: String type. If it is datetime string, the value of 'suffix_is_datetime_str' should be set to True.
+	   extension: String type. Designation the extension of file. Default value is '.csv' means CSV file.
+	"""
 
-	def __init__(self, filepath, prefix='', prefix_is_datetime_str=False, suffix='', suffix_is_datetime_str=False, extension='.csv', **attributes):
+	def __init__(self, filepath, prefix='', prefix_is_datetime_str=False, suffix='', suffix_is_datetime_str=False, extension='.csv'):
 		self.__filepath = filepath
 		self.__prefix = prefix
 		self.__suffix = suffix
 		self.__prefix_is_datetime_str = prefix_is_datetime_str
 		self.__suffix_is_datetime_str = suffix_is_datetime_str
 		self.__extension = extension
-		self.__attributes = attributes
+		self.__allfiles = [f for f in os.listdir(self.__filepath) if os.path.isfile(os.path.join(self.__filepath, f))]
 
 	def filepath(self):
 		return self.__filepath
@@ -78,7 +79,7 @@ class DataFile:
 		self.__suffix = datetime.strftime(oDatetime, pattern)
 		return oldone, self.__prefix
 
-	def countFiles(self, prefix=None, suffix=None, extension=None, **attributes):
+	def countFiles(self, prefix=None, suffix=None, extension=None):
 		if prefix is None:
 			prefix = self.__prefix
 		if suffix is None:
@@ -86,12 +87,25 @@ class DataFile:
 		if extension is None:
 			extension = self.__extension
 
-		files = [f for f in os.listdir(self.__filepath) if os.path.isfile(os.path.join(self.__filepath,f))]
-		filenames = [f[:-len(extension)] for f in files]
-		fileappliedprefix = [f for f in filenames if f.startswith(prefix)]
-		fileappliedsuffix = [f for f in fileappliedprefix if f.endswith(suffix)]
+		filenames = [f[:-len(extension)] for f in self.__allfiles]
+		filteredfilenames = filterFilenameBySuffix(filterFilenameByPrefix(filenames, prefix), suffix)
 
 		return len(fileappliedsuffix)
 
+	def searchNFiles(pattern, N, ascending=True, **attribute):
 
-def searchNFiles(fld, pattern, N, ascending=True, attribute)
+
+
+def filterFilenameByPrefix(filelist, prefix):
+	if not isinstance(filelist, list):
+		raise TypeError("filelist must be a list")
+	if not isinstance(prefix, str):
+		raise TypeError("prefix must be a str")
+	return [f for f in filelist if f.startswith(prefix)]
+
+def filterFilenameBySuffix(filelist, suffix):
+	if not isinstance(filelist, list):
+		raise TypeError("filelist must be a list")
+	if not isinstance(suffix, str):
+		raise TypeError("suffix must be a str")
+	return [f for f in filelist if f.endswith(suffix)]
